97d6694dbcdce14ae88125a006008e14b96995ae
koanco@hotmail.com <koanco@hotmail.com>
2017-10-02 21:31:54 -0500

    commit-johann cambio en comentario inicial

--- app/src/main/java/com/example/koanc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/koanc/myapplication/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.koanc.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//Johann Polania Gonzalez
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 }
